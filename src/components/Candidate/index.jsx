925bfb562038a2b0fbbcc9e4e5894726d8ea37ac
Han Frisonsova <hanafrisonsova@gmail.com>
2025-05-10 15:11:44 +0200

    zobrazeni kandidáta

--- src/components/Candidate/index.jsx
+++ src/components/Candidate/index.jsx
@@ -1,11 +1,13 @@
 import './style.css';
 
-export const Candidate = ({ name, avatar }) => {
+export const Candidate = ({ name, avatar, onSelect }) => {
   return (
     <div className="candidate">
       <h3 className="candidate__name">{name}</h3>
       <img className="candidate__avatar" src={avatar} />
-      <button className="btn-vote">Vybrat</button>
+      <button className="btn-vote" onClick={() => onSelect(name)}>
+        Vybrat
+      </button>
     </div>
   );
 };
